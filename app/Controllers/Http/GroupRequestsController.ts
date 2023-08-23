a109ace81fba999a13c2f4612fada44bc3e8c18d
joao <joaodeus400@gmail.com>
2023-08-23 09:03:00 -0300

    Tratando cenário de solitação existentes para a mesa

--- app/Controllers/Http/GroupRequestsController.ts
+++ app/Controllers/Http/GroupRequestsController.ts
@@ -1,5 +1,6 @@
 // import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
+import BadRequestException from 'App/Exceptions/BadRequestException';
 import GroupRequest from 'App/Models/GroupRequest';
 
 
@@ -8,8 +9,13 @@ export default class GroupRequestsController {
     const groupId = request.param('groupId') as number
     const userId = auth.user!.id
 
+    const existingGroupRequest = await GroupRequest.query().where('groupId', groupId).andWhere('userId', userId).first()
+    if (existingGroupRequest) throw new BadRequestException('group request already exists', 409)
+
     const groupRequest = await GroupRequest.create({groupId, userId})
     await groupRequest.refresh()
     return response.created({ groupRequest  })
   }
 }
+
+
--- test/groups/group_request.spec.ts
+++ test/groups/group_request.spec.ts
@@ -10,7 +10,7 @@ let token = ''
 let user = {} as User
 
 test.group('Group Request', (group) => {
-  test.only('it should create a group request', async (assert) => {
+  test('it should create a group request', async (assert) => {
    const { id } = await UserFactory.create()
    const group = await GroupFactory.merge({ master: id }).create()
    const { body } = await supertest(BASE_URL).post(`/groups/${group.id}/requests`).set('Authorization', `Bearer ${token}`).send({}).expect(201)
@@ -20,6 +20,17 @@ test.group('Group Request', (group) => {
    assert.equal(body.groupRequest.userId, user.id)
    assert.equal(body.groupRequest.groupId, group.id)
    assert.equal(body.groupRequest.status, 'PENDING')
+  })
+
+  test.only('it should return 409 when group request already exists', async (assert) => {
+     const { id } = await UserFactory.create()
+     const group = await GroupFactory.merge({ master: id }).create()
+     await supertest(BASE_URL).post(`/groups/${group.id}/requests`).set('Authorization', `Bearer ${token}`).send({})
+
+     const { body } = await supertest(BASE_URL).post(`/groups/${group.id}/requests`).set('Authorization', `Bearer ${token}`).send({}).expect(409)
+
+     assert.equal(body.code, 'BAD_REQUEST')
+     assert.equal(body.status, 409)
   })
 
   group.before(async () => {
